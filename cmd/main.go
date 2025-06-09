7dc2b8482b2c73accac3a590ec4790657a80076b
Andres <arias.andres2321@gmail.com>
2025-06-09 13:41:28 -0400

    created chi router and deployed lambda with health check handler endpoint

--- cmd/main.go
+++ cmd/main.go
@@ -1,18 +1,19 @@
 package main
 
 import (
-	"github.com/aws/aws-lambda-go/lambda"
-	"github.com/awslabs/aws-lambda-go-api-proxy/chi"
 	"leaguies_backend/router"
+
+	"github.com/aws/aws-lambda-go/lambda"
+	chiadapter "github.com/awslabs/aws-lambda-go-api-proxy/chi"
 )
 
-var adapter *chiadapter.ChiLambda
+var adapter *chiadapter.ChiLambdaV2
 
 func init() {
 	r := router.NewRouter()
-	adapter = chiadapter.New(r)
+	adapter = chiadapter.NewV2(r)
 }
 
 func main() {
-	lambda.Start(adapter.ProxyWithContext)
+	lambda.Start(adapter.ProxyWithContextV2)
 }
